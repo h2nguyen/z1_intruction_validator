44b22441fe7536b75ef50d43a306ad46ee3086a3
Hai Nguyen <h.nguyen@fu-berlin.de>
2013-09-30 19:14:55 +0200

    - added some basic binary operations

--- src/main/java/core/helpers/BinaryHelper.java
+++ src/main/java/core/helpers/BinaryHelper.java
@@ -9,11 +9,82 @@ package core.helpers;
  */
 public class BinaryHelper {
 
-	/**
-	 * 
-	 */
-	public BinaryHelper() {
-		// TODO Auto-generated constructor stub
+	public static String convDecStringToBinString(String decString) {
+		return Integer.toBinaryString(Integer.parseInt(decString));
+	}
+	
+	public static int convDecStringToBinInteger(String decString) {
+		return Integer.parseInt(Integer.toBinaryString(Integer.parseInt(decString)));
+	}
+	
+	public static String convDecIntegerToBinString(int decInteger) {
+		if(decInteger < 0) {
+			return "-"+Integer.toBinaryString(-decInteger);
+		}
+		return Integer.toBinaryString(decInteger);
+	}
+	
+	public static int convDecIntegerToBinInteger(int decInteger) {
+		if(decInteger < 0) {
+			return -Integer.parseInt(Integer.toBinaryString(-decInteger));
+		}
+		return Integer.parseInt(Integer.toBinaryString(decInteger));
 	}
 
+	public static String convBinStringToDecString(String binString) {		
+		return Integer.toString(Integer.parseInt(binString,2));
+	}
+	
+	public static int convBinStringToDecInteger(String binString) {
+		return Integer.parseInt(binString,2);
+	}
+
+	public static String convBinIntegerToBinString(int binInteger) {
+		return String.valueOf(binInteger);
+	}
+	
+	public static String convBinIntegerToDecString(int binInteger) {		
+		return String.valueOf(Integer.parseInt(String.valueOf(binInteger), 2));
+	}
+	
+	public static int convBinIntegerToBinInteger(int binInteger) {		
+		return Integer.parseInt(String.valueOf(binInteger), 2);
+	}
+	
+	public static int addBinariesInt(int bin1, int bin2) {
+		bin1 = BinaryHelper.convBinIntegerToBinInteger(bin1);
+		bin2 = BinaryHelper.convBinIntegerToBinInteger(bin2);
+		return BinaryHelper.convDecIntegerToBinInteger(bin1+bin2);
+	}
+	
+	public static int subBinariesInt(int bin1, int bin2) {
+		bin1 = BinaryHelper.convBinIntegerToBinInteger(bin1);
+		bin2 = BinaryHelper.convBinIntegerToBinInteger(bin2);
+		
+		if(bin2 > bin1) {
+			return -BinaryHelper.convDecIntegerToBinInteger(bin2-bin1);
+		}
+		
+		return BinaryHelper.convDecIntegerToBinInteger(bin1-bin2);
+	}
+	
+	public static int mulBinariesInt(int bin1, int bin2) {
+		bin1 = BinaryHelper.convBinIntegerToBinInteger(bin1);
+		bin2 = BinaryHelper.convBinIntegerToBinInteger(bin2);
+		
+		return BinaryHelper.convDecIntegerToBinInteger(bin1*bin2);
+	}
+	
+	/**
+	 * TODO: needs to be implemented
+	 * @param bin1
+	 * @param bin2
+	 * @return
+	 */
+	public static int divBinariesInt(int bin1, int bin2) {
+		bin1 = BinaryHelper.convBinIntegerToBinInteger(bin1);
+		bin2 = BinaryHelper.convBinIntegerToBinInteger(bin2);
+		
+		return BinaryHelper.convDecIntegerToBinInteger(bin1/bin2);
+	}
 }
---
+++ src/test/java/BinaryNumberTestCase.java
@@ -0,0 +1,48 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import core.helpers.BinaryHelper;
+
+/**
+ * 
+ */
+
+/**
+ * @author hnguyen
+ *
+ */
+public class BinaryNumberTestCase extends BinaryHelper {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		String numStringDec = "15";
+		String numStringBin = "-111";
+		int numIntDec = -15;
+		int numIntBin2 = 1111;
+		int numIntBin1 = 111;
+		
+//		System.out.println(BinaryHelper.convBinIntegerToBinString(numIntBin2));
+//		System.out.println(BinaryHelper.convBinIntegerToDecString(numIntBin2));
+//		System.out.println(BinaryHelper.convBinStringToDecInteger(numStringBin));
+//		System.out.println(BinaryHelper.convBinStringToDecString(numStringBin));
+//		System.out.println(BinaryHelper.convDecIntegerToBinInteger(numIntDec));
+//		System.out.println(BinaryHelper.convDecIntegerToBinString(numIntDec));
+//		System.out.println(BinaryHelper.convDecStringToBinInteger(numStringDec));
+//		System.out.println(BinaryHelper.convDecStringToBinString(numStringBin));
+		
+		System.out.println(BinaryHelper.addBinariesInt(numIntBin1, numIntBin1));
+		System.out.println(BinaryHelper.subBinariesInt(numIntBin2, numIntBin1));
+		System.out.println(BinaryHelper.mulBinariesInt(numIntBin2, numIntBin1));
+		System.out.println(BinaryHelper.divBinariesInt(numIntBin2, numIntBin1));
+	}
+
+}
