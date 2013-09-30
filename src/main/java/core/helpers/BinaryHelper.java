f805e17072a37f11f85774f8970b3a22fc0d90c9
Hai Nguyen <h.nguyen@fu-berlin.de>
2013-09-30 15:56:06 +0200

    - initial commit

---
+++ src/main/java/core/helpers/BinaryHelper.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package core.helpers;
+
+/**
+ * @author hnguyen
+ *
+ */
+public class BinaryHelper {
+
+	/**
+	 * 
+	 */
+	public BinaryHelper() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
